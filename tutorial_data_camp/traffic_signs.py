21706afa527804030b5000e0cb659f32459e6680
Reynaldo Gil <gilcu2@gmail.com>
2019-08-10 08:44:34 +0100

    scaling

--- tutorial_data_camp/traffic_signs.py
+++ tutorial_data_camp/traffic_signs.py
@@ -58,12 +58,36 @@ def show_some_images_with_features(images_array: np.array):
     for i in range(len(traffic_signs)):
         plt.subplot(1, 4, i + 1)
         plt.axis('off')
-        plt.imshow(images[traffic_signs[i]])
+        plt.imshow(images_array[traffic_signs[i]])
         plt.subplots_adjust(wspace=0.5)
         plt.show()
         print("shape: {0}, min: {1}, max: {2}".format(images[traffic_signs[i]].shape,
                                                       images[traffic_signs[i]].min(),
                                                       images[traffic_signs[i]].max()))
+
+
+def scale_images(images_array: np.array) -> np.array:
+    images28 = [skimage.transform.resize(image, (28, 28)) for image in images]
+    return np.array(images28)
+
+
+def show_some_images_for_each_class(images_array: np.array, unique_labels: set):
+    plt.figure(figsize=(28, 28))
+    plt.subplots_adjust(hspace=0.75)
+
+    i = 1
+
+    for label in unique_labels:
+        image = images_array[labels.index(label)]
+        plt.subplot(8, 8, i)
+
+        plt.axis('off')
+
+        plt.title("Label {0} {1} \n{2}".format(label, labels.count(label), image.shape))
+        i += 1
+        plt.imshow(image)
+
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -82,4 +106,6 @@ if __name__ == '__main__':
 
     # plot_labels_histogram(labels, len(unique_labels))
 
-    show_some_images(images_array)
+    images_28 = scale_images(images_array)
+
+    show_some_images_for_each_class(images_28, unique_labels)
